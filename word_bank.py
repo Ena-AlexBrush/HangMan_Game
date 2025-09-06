4f4988990a6f127a86089cd0c9f671a886985a08
Chanchal M <160550129+Ena-AlexBrush@users.noreply.github.com>
2025-09-06 16:44:23 -0700

    Create word_bank.py

---
+++ word_bank.py
@@ -0,0 +1,8 @@
+words_list = [
+    "python", "hangman", "developer", "banana", "elephant",
+    "mountain", "chocolate", "keyboard", "giraffe", "notebook",
+    "sunshine", "computer", "window", "pencil", "internet",
+    "language", "library", "science", "football", "umbrella",
+    "holiday", "diamond", "volcano", "penguin", "sandwich",
+    "airplane", "printer", "backpack", "calendar", "bicycle"
+]
